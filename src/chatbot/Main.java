0b82c7a81da59cb832d14ce9fe9de7975310fb3a
Virvi Huta <hutavirvi07@gmail.com>
2026-06-10 10:12:41 +0200

    Update Main to use no-arg AnthropicClient constructor

--- src/chatbot/Main.java
+++ src/chatbot/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        AnthropicClient client = new AnthropicClient("YOUR_API_KEY_HERE");
+        AnthropicClient client = new AnthropicClient();
         ConversationHistory history = new ConversationHistory();
         Scanner sc = new Scanner(System.in);
 
