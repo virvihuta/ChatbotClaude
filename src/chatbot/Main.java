748dd230c7855404cf19780eb42d6b3558a48df7
Virvi Huta <hutavirvi07@gmail.com>
2026-06-10 10:04:55 +0200

    Add Main class for console testing of logic layer

--- src/chatbot/Main.java
+++ src/chatbot/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package chatbot;
+
+import java.util.Scanner;
+
+/**
+ * Temporary console test for the logic layer.
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        AnthropicClient client = new AnthropicClient("YOUR_API_KEY_HERE");
+        ConversationHistory history = new ConversationHistory();
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Type a message (or 'quit'):");
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("quit")) break;
+            history.add(new ChatMessage("user", input));
+            String reply = client.sendMessage(history.getMessages());
+            history.add(new ChatMessage("assistant", reply));
+            System.out.println("Claude: " + reply);
         }
     }
 }
